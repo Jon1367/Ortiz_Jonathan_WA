c2ccd7adaa5436d2435c5e751b0eebf874d3d6ea
Jonathan Ortiz <jonathano1367@yahoo.com>
2014-10-05 11:25:00 -0700

    asfdasfd
    ss

---
+++ lecture/Day3-StudentFiles_ecc40b27-16c3-4e81-8e0c-9d442c7e645d/1-HTML5 Video/js/videoControl.js
@@ -0,0 +1,27 @@
+var vid:
+
+window.onload = function  () {
+	// body...
+	vid = document.getElementById("vid");
+
+
+}
+
+
+//Create a function - that when we click on the video itself we will pause or play the video
+
+function pauseToggle(){
+
+		//test if the video is currently playign or paused
+		// posted propter - boolen
+
+		if(vid.paused){
+
+			//if paused then play the video
+			vid.play();
+
+		}else{
+
+			//video is the
+		}
+}
---
+++ lecture/Day3-StudentFiles_ecc40b27-16c3-4e81-8e0c-9d442c7e645d/4-jQuery/js/ShowHide.js
@@ -0,0 +1,14 @@
+$(function  (argument) {
+	// encapuslates the varivales and protects them
+
+	$("#show").click(function(){
+
+		$("#theDiv").show();
+
+
+	})
+
+	
+
+
+});
